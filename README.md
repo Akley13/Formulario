99591f23f803ee89759d4de748a07b296db622b1
Akley Alves <akleyalvescr@gmail.com>
2023-02-28 22:28:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Formulario
+ HTML | JavaScript | JQuery
---
+++ funcoes.js
@@ -0,0 +1,8 @@
+// $ - É uma representação das funções do jquery - escopo
+// Function - É uma palavra reservada e ao declarar (váriaves)
+
+$(function(){
+alert(" Alerta  de Integração com JavaScript | Exemplo");
+console.log("Escrever texto no console do Navegador.");
+
+});
