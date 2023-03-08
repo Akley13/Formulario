b5144c4f5be86b4a6d033503c6de7df705f74cee
Akley Alves <akleyalvescr@gmail.com>
2023-03-07 21:37:37 -0300

    upd

--- Form/funcoes.js
+++ Form/funcoes.js
@@ -2,7 +2,16 @@
 // Function - É uma palavra reservada e ao declarar (váriaves)
 
 $(function(){
-alert(" Alerta  de Integração com JavaScript | Exemplo");
-console.log("Escrever texto no console do Navegador.");
+console.log('Início da função para fazer registro de dados.')
+var operacao = "A"; // A - Adição/Inclusão; E - "Edição"
+//var indice_selecionado
+//var indice_selecionado
+var indice_selecionado = -1;
+
+// Recuperar os dados de alunos inseridos pelo formulário.
+var tblAluno = localStorage.getItem("alunos");
+console.log(tblAluno);
+
+
 
 });
